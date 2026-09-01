9af211d6825a0c1844b145d379673fd4a39e3158
Phạm Ngọc Bảo An <anpham79.dev@gmail.com>
2026-09-01 11:35:53 +0700

    feat: base layout

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from './Header.vue'
+</script>
+
+<template>
+    <div>
+        <Header />
+
+        <main>
+            <div class="container-xl">
+                <slot />
+            </div>
+        </main>
+    </div>
+</template>
